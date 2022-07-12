1aeeee1db9365b553b9e75d15ec2333b01733339
jonprovan <106700125+jonprovan@users.noreply.github.com>
2022-07-12 17:51:09 -0400

    Frontend Upload

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component';
+import { LoginRegisterComponent } from './login-register/login-register.component';
+import { ManageDevicesComponent } from './manage-devices/manage-devices.component';
+import { ManagePlansComponent } from './manage-plans/manage-plans.component';
+import { ViewBillComponent } from './view-bill/view-bill.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home', component: HomeComponent
+  },
+  {
+    path: 'loginRegister', component: LoginRegisterComponent
+  },
+  {
+    path: 'dashboard', component: DashboardComponent
+  },
+  {
+    path: 'managePlans', component: ManagePlansComponent
+  },
+  {
+    path: 'manageDevices', component: ManageDevicesComponent
+  },
+  {
+    path: 'viewBill', component: ViewBillComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginRegisterComponent } from './login-register/login-register.component';
+import { ManagePlansComponent } from './manage-plans/manage-plans.component';
+import { ManageDevicesComponent } from './manage-devices/manage-devices.component';
+import { ViewBillComponent } from './view-bill/view-bill.component';
+import { PlanComponent } from './plan/plan.component';
+import { DeviceComponent } from './device/device.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavBarComponent,
+    HomeComponent,
+    DashboardComponent,
+    LoginRegisterComponent,
+    ManagePlansComponent,
+    ManageDevicesComponent,
+    ViewBillComponent,
+    PlanComponent,
+    DeviceComponent
   ],
   imports: [
     BrowserModule,
