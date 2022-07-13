bb7fead66d09a3ca70b8caa21b5f36041b54df0a
jonprovan <106700125+jonprovan@users.noreply.github.com>
2022-07-13 17:53:28 -0400

    Frontend edits

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -2,17 +2,21 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HomeComponent } from './home/home.component';
-import { LoginRegisterComponent } from './login-register/login-register.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 import { ManageDevicesComponent } from './manage-devices/manage-devices.component';
 import { ManagePlansComponent } from './manage-plans/manage-plans.component';
 import { ViewBillComponent } from './view-bill/view-bill.component';
 
 const routes: Routes = [
   {
-    path: 'home', component: HomeComponent
+    path: '', component: HomeComponent
   },
   {
-    path: 'loginRegister', component: LoginRegisterComponent
+    path: 'login', component: LoginComponent
+  },
+  {
+    path: 'register', component: RegisterComponent
   },
   {
     path: 'dashboard', component: DashboardComponent
--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -7,12 +7,15 @@ import { HeaderComponent } from './header/header.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HomeComponent } from './home/home.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { LoginRegisterComponent } from './login-register/login-register.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 import { ManagePlansComponent } from './manage-plans/manage-plans.component';
 import { ManageDevicesComponent } from './manage-devices/manage-devices.component';
 import { ViewBillComponent } from './view-bill/view-bill.component';
 import { PlanComponent } from './plan/plan.component';
 import { DeviceComponent } from './device/device.component';
+import { CurrentPlanComponent } from './current-plan/current-plan.component';
+import { AvailablePlanComponent } from './available-plan/available-plan.component';
 
 @NgModule({
   declarations: [
@@ -21,12 +24,15 @@ import { DeviceComponent } from './device/device.component';
     NavBarComponent,
     HomeComponent,
     DashboardComponent,
-    LoginRegisterComponent,
+    LoginComponent,
+    RegisterComponent,
     ManagePlansComponent,
     ManageDevicesComponent,
     ViewBillComponent,
     PlanComponent,
-    DeviceComponent
+    DeviceComponent,
+    CurrentPlanComponent,
+    AvailablePlanComponent
   ],
   imports: [
     BrowserModule,
