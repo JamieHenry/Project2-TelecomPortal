e09b4b6ed5c3cd153e8c04bf6bc8cec481174b5f
jonprovan <106700125+jonprovan@users.noreply.github.com>
2022-07-15 15:54:18 -0400

    Add device and header changes

--- web/src/app/manage-devices/manage-devices.component.ts
+++ web/src/app/manage-devices/manage-devices.component.ts
@@ -16,21 +16,41 @@ export class ManageDevicesComponent implements OnInit {
     
   }
 
-  makeDefault: boolean = true;
-  makeApple: boolean = false;
-  makeRIM: boolean = false;
-  makeSamsung: boolean = false;
+  models: any = { model1: "", model2: "", model3: "", model4: ""};
+  modelsDefault: any = { model1: "", model2: "", model3: "", model4: ""};
+  modelsApple: any = { model1: "iPhone 11", model2: "iPhone 12", model3: "iPhone 13 Pro Max", model4: "iPhone SE" }
+  modelsRIM: any = { model1: "BlackBerry 5G", model2: "BlackBerry KeyOne", model3: "BlackBerry Key2", model4: "BlackBerry Priv" }
+  modelsSamsung: any = { model1: "Galaxy A", model2: "Galaxy Note", model3: "Galaxy S", model4: "Galaxy Z" }
+  modelsOther: any = { model1: "", model2: "", model3: "", model4: ""};
+  
   makeOther: boolean = false;
   modelOther: boolean = false;
 
-  selectMake(makeValue: string): void {
+  selectMake(optionValue: string): void {
 
-    this.makeDefault = makeValue == "" ? true : false;
-    this.makeApple = makeValue == "Apple" ? true : false;
-    this.makeRIM = makeValue == "RIM" ? true : false;
-    this.makeSamsung = makeValue == "Samsung" ? true : false;
-    this.makeOther = makeValue == "Other" ? true : false;
-    this.modelOther = makeValue == "Other" ? true : false;
+    this.makeOther = false;
+    this.modelOther = false;
+
+    switch (optionValue) {
+      case "":
+        this.models = this.modelsDefault;
+        break;
+      case "Apple":
+        this.models = this.modelsApple;
+        break;
+      case "RIM":
+        this.models = this.modelsRIM;
+        break;
+      case "Samsung":
+        this.models = this.modelsSamsung;
+        break;
+      case "Other":
+        this.makeOther = true;
+        this.modelOther = true;
+        this.models = this.modelsOther;
+        break; 
+    }
+    
   }
 
   selectModel(optionValue: string): void {
