7390d1aff498d2166af338e5919931af5083b2da
JamieHenry <jhenry@skillstorm.com>
2022-07-28 10:43:00 -0400

    Hotfix

--- backend/src/main/java/com/telecom/beans/User.java
+++ backend/src/main/java/com/telecom/beans/User.java
@@ -16,6 +16,8 @@ import org.springframework.security.core.userdetails.UserDetails;
 @Entity
 public class User implements UserDetails {
 
+	private static final long serialVersionUID = 1381921589313502020L;
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
